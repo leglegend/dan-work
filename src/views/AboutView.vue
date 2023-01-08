6c97c86cc5b93086b4bd15e1c7b450eb40d167fd
nite <532702231@qq.com>
2023-01-08 15:25:11 +0800

    随机数

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -32,7 +32,8 @@
         </div>
       </el-form-item>
     </el-form>
-    <el-button type="primary">生成</el-button>
+    <el-button type="primary" @click="onGenerate">生成</el-button>
+    <div>结果：{{ result }}</div>
   </div>
 </template>
 <script setup lang="ts">
@@ -48,6 +49,80 @@ const form = ref<{
   type: 0,
   repeat: false,
 })
+
+form.value.textarea = localStorage.getItem('textarea') || ''
+
+const result = ref('')
+
+function onGenerate() {
+  if (!form.value.count) return
+  if (form.value.type === 0 && form.value.max && form.value.min) {
+    if (
+      form.value.count >
+      Number(form.value.max!) - Number(form.value.min!) + 1
+    )
+      return
+
+    const list: number[] = []
+    for (let i = 0; i < form.value.count; i++) list.push(random())
+
+    if (form.value.repeat) result.value = list.join(' ')
+    else {
+      calcRepeat(list)
+    }
+  } else if (form.value.type === 1 && form.value.textarea) {
+    localStorage.setItem('textarea', form.value.textarea)
+    const text = form.value.textarea
+      .split(' ')
+      .map((item) => Number(item))
+      .filter((item) => !isNaN(item))
+
+    if (form.value.count > text.length) return
+
+    const list: number[] = []
+    for (let i = 0; i < form.value.count; i++) list.push(random2(text.length))
+
+    if (form.value.repeat)
+      result.value = list.map((item) => text[item]).join(' ')
+    else {
+      result.value = calcRepeat2(list, text.length)
+        .map((item) => text[item])
+        .join(' ')
+    }
+  }
+}
+
+function random() {
+  return parseInt(
+    String(
+      Math.random() * (Number(form.value.max!) - Number(form.value.min!) + 1) +
+        Number(form.value.min!)
+    ),
+    10
+  )
+}
+
+function random2(max: number) {
+  return parseInt(String(Math.random() * max), 10)
+}
+
+function calcRepeat(list: number[]) {
+  const newList = new Array(...new Set(list))
+  if (newList.length >= Number(form.value.count!)) result.value = list.join(' ')
+  else {
+    newList.push(random())
+    calcRepeat(newList)
+  }
+}
+
+function calcRepeat2(list: number[], max: number): number[] {
+  const newList = new Array(...new Set(list))
+  if (newList.length >= Number(form.value.count!)) return newList
+  else {
+    newList.push(random2(max))
+    return calcRepeat2(newList, max)
+  }
+}
 </script>
 
 <style scoped lang="scss">
